abcbb81ecb0d4e005d0139c0cab36a1a40565625
boonsboos <mrsherobrinenaomi@gmail.com>
2023-12-27 15:51:56 +0100

    [fix] remove comma

--- src/main/java/cgroup2/cadmycode/gui/SceneManager.java
+++ src/main/java/cgroup2/cadmycode/gui/SceneManager.java
@@ -29,7 +29,7 @@ public class SceneManager {
         this.scenes = new HashMap<>(Map.ofEntries(
                 Map.entry(SceneType.WEBCAST, new WebcastScene(mainStage)),
                 Map.entry(SceneType.DASHBOARD, new DashboardScene(mainStage)),
-                Map.entry(SceneType.ABOUT, new AboutScene(mainStage)),
+                Map.entry(SceneType.ABOUT, new AboutScene(mainStage))
         ));
 
         this.scenes.get(splash).show();

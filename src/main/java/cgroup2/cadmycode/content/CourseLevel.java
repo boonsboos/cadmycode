d56f5748bb30df730d12e4786feb70b7c6bb2e62
boonsboos <mrsherobrinenaomi@gmail.com>
2024-01-02 04:02:08 +0100

    [fix] add helper methods to course level and setters to module

--- src/main/java/cgroup2/cadmycode/content/CourseLevel.java
+++ src/main/java/cgroup2/cadmycode/content/CourseLevel.java
@@ -3,5 +3,31 @@ package cgroup2.cadmycode.content;
 public enum CourseLevel {
     BEGINNER,
     INTERMEDIATE,
-    EXPERT
+    EXPERT;
+
+    public int asInt() {
+        switch (this) {
+            case BEGINNER:
+                return 0;
+            case INTERMEDIATE:
+                return 1;
+            case EXPERT:
+                return 2;
+            default:
+                return 0;
+        }
+    }
+
+    public static CourseLevel fromInt(int a) {
+        switch (a) {
+            case 0:
+                return BEGINNER;
+            case 1:
+                return INTERMEDIATE;
+            case 2:
+                return EXPERT;
+            default:
+                return BEGINNER;
+        }
+    }
 }

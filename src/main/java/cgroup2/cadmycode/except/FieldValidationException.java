6d3c89e835c844c2cadffb9b7bdd9693defc42c6
FB99FIRE <felix99@hotmail.nl>
2024-03-28 15:04:10 +0100

    documenting except

--- src/main/java/cgroup2/cadmycode/except/FieldValidationException.java
+++ src/main/java/cgroup2/cadmycode/except/FieldValidationException.java
@@ -1,6 +1,13 @@
 package cgroup2.cadmycode.except;
 
+/**
+ * a custom exception class for handling FieldValidationExceptions
+ */
 public class FieldValidationException extends Exception {
+    /**
+     * creates an instance of {@link FieldValidationException}
+     * @param message the message given as parameter for the method call
+     */
     public FieldValidationException(String message) {
         super(message);
     }
